15972b31c8b8d4dc3ec7c1e37bc3343dafad9250
fschirinzi <5647342+fschirinzi@users.noreply.github.com>
2020-10-08 10:11:18 +0200

    force json response

--- config/laramote.php
+++ config/laramote.php
@@ -2,6 +2,7 @@
 <?php
 
 use Fschirinzi\LaraMote\Http\Middleware\EnsureUserIsAuthorized;
+use Fschirinzi\LaraMote\Http\Middleware\ForceJsonResponse;
 
 return [
 
@@ -18,6 +19,7 @@ return [
 
     'middleware' => [
         'api',
+        ForceJsonResponse::class,
         EnsureUserIsAuthorized::class,
     ],
 
