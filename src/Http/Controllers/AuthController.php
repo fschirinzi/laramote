bfae6e5731a275837e504a8a01ad72ae99059944
fschirinzi <5647342+fschirinzi@users.noreply.github.com>
2021-03-19 11:27:19 +0100

    get default auth model from config

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -12,12 +12,17 @@ class AuthController
         $id = $request->get('key_value');
         $key = $request->get('key', 'id');
         $remember = $request->get('remember', true);
-        $modelClass = $request->get('model', 'App\User');
+        $modelClass = $request->get('model', $this->getDefaultAuthModel());
 
         $modelWithNamespace = LaraMote::guess_class_and_namespace($modelClass);
 
         $model = (new $modelWithNamespace)->where($key, $id)->firstOrFail();
 
         return auth()->login($model, $remember);
+    }
+
+    protected function getDefaultAuthModel(){
+        $authPasswordDefault = config('auth.defaults.passwords');
+        return config("auth.providers.{$authPasswordDefault}.model");
     }
 }
